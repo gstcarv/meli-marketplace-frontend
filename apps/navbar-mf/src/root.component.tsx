b9fa3174bd1361999560bf8a9d27cb5084164b16
Gusta 😎 <gstcarv@gmail.com>
2024-10-10 13:25:29 -0300

    chore: initial microfrontend setup

---
+++ apps/navbar-mf/src/root.component.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@meli/ui";
+
+export default function Root() {
+    return (
+        <section>
+            <Button appName="asdad">hello world</Button>
+        </section>
+    );
+}
