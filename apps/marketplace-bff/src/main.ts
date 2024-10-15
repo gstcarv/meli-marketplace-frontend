5ea520b09209fa61544a1304feb8f12001990aa1
Gusta 😎 <gstcarv@gmail.com>
2024-10-15 11:09:24 -0300

    chore(bff): added vercel build configurations

--- apps/marketplace-bff/src/main.ts
+++ apps/marketplace-bff/src/main.ts
@@ -1,11 +1,14 @@
 import 'dotenv/config';
 
-import express from 'express';
+import express, { Express } from 'express';
+import path from 'path';
 import { config } from './config';
 import { errorHandler } from './middlewares/error-handler.middleware';
 import { itemsRouter } from './modules/items/routes';
 
-const server = express();
+const server: Express = express();
+
+server.get('/', (req, res) => res.send('ok'));
 
 server.use(itemsRouter);
 
@@ -14,3 +17,5 @@ server.use(errorHandler);
 server.listen(config.port, () => {
     console.log(`🚀 app running on ${config.port}`);
 });
+
+export default server;
