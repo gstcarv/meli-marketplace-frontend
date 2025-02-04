f5fbfca39bfad0a029fd305fba2c12d960a812fe
Gusta 😎 <gstcarv@gmail.com>
2025-02-04 15:35:37 -0300

    fix(vitals): lazy loading font to improve cls

--- packages/design-tokens/src/tokens/fontWeight.ts
+++ packages/design-tokens/src/tokens/fontWeight.ts
@@ -3,6 +3,5 @@ import { createTokens } from '../lib/create-tokens';
 export const fontWeight = createTokens({
     bold: 500,
     medium: 400,
-    regular: 300,
-    thin: 200
+    regular: 300
 } as const);
